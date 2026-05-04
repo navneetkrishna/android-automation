59d7e378f695101eb57c59cd2f8dd6d8fdbc3460
Navneet Krishna Srivastava <navneetkrishna4@gmail.com>
2026-05-04 14:59:26 +0530

    Update test_date_widgets.py
    Added test date and time widget script

--- tests/views/test_date_widgets.py
+++ tests/views/test_date_widgets.py
@@ -2,24 +2,43 @@ import pytest
 from pages.home_page import HomePage
 from pages.views.date_widgets_page import DateWidgetPage
 from pages.views.views_home_page import ViewsHomePage
-import time
-from datetime import datetime
 
-@pytest.mark.date
-def test_date(driver):
-    homepage = HomePage(driver)
 
-    homepage.navigate_to('Views')
+class TestDateWidget:
 
-    views = ViewsHomePage(driver)
-    views.navigate_to('Date Widgets')
+    @pytest.mark.date
+    def test_date_setting(self, driver):
+        homepage = HomePage(driver)
 
-    date_page = DateWidgetPage(driver)
+        homepage.navigate_to('Views')
 
-    set_date = "1-1-2020"
-    date_page.set_date_to(set_date)
+        views = ViewsHomePage(driver)
+        views.navigate_to('Date Widgets')
 
-    current_date = date_page.confirm_date()
+        date_page = DateWidgetPage(driver)
 
-    assert current_date == set_date, 'Set date and Current date did not match'
+        set_date = "1-1-2020"
+        date_page.set_date_to(set_date)
+
+        current_date = date_page.confirm_date_time()[0]
+
+        assert current_date == set_date, 'Set date and Current date did not match'
+
+    @pytest.mark.time
+    def test_dialer_time_setting(self, driver):
+        homepage = HomePage(driver)
+
+        homepage.navigate_to('Views')
+
+        views = ViewsHomePage(driver)
+        views.navigate_to('Date Widgets')
+
+        date_page = DateWidgetPage(driver)
+
+        set_time = "1-10-pm"
+        date_page.set_dialer_time_to(set_time)
+
+        current_time = date_page.confirm_date_time()[1]
+
+        assert current_time == set_time, 'Set time and Current time did not match'
 
